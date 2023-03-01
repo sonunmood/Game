847184eb8936b83e8498d76d26d21a0b45692ed2
Sonun <usubalievasonun@gmail.com>
2023-03-01 14:12:47 +0600

    initial commit

---
+++ Game/Game.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+protocol Menu {
+    
+    func startGAme ()
+    
+    func endGame ()
+}
+
+
+class Game: Menu {
+    
+    func startGAme() {
+        return
+    }
+    
+    func endGame() {
+        return
+    }
+}
--- Game/main.swift
+++ Game/main.swift
@@ -1,11 +1,41 @@
-//
-//  main.swift
-//  Game
-//
-//  Created by Sonun on 1/3/23.
-//
-
 import Foundation
 
-print("Hello, World!")
+var field1: [String] =
+[" ", " ", " "]
+
+var field2: [String] =
+[" ", " ", " "]
+
+var field3: [String] =
+[" ", " ", " "]
+
+
+for i in 1...9 {
+    let play = readLine()!
+    var symbol = ""
+    var index = 0
+    
+    if ((play.last?.isNumber) != nil) {
+        index = Int(String(play.last! )) ?? 0
+    }
+    
+    if i % 2 != 0 {
+        symbol = "X"
+    } else {
+        symbol = "O"
+    }
+    
+    if play.first == "1" {
+        
+        field1[index - 1] = symbol
+    } else if play.first == "2" {
+        field2[index - 1] = symbol
+    } else if play.first == "3" {
+        field3[index - 1] = symbol
+    }
+    
+    print(field1)
+    print(field2)
+    print(field3)
+}
 
